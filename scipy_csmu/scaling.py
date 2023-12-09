5f752a8a35daa045301f64796ac7ae6659a1a861
Martin Spišák <martin.spisak@glami.cz>
2023-12-09 19:18:35 +0100

    [scaling] unify parameters denoting matrices

--- scipy_csmu/scaling.py
+++ scipy_csmu/scaling.py
@@ -4,21 +4,21 @@ import numpy as np
 from scipy import sparse as sp
 
 
-def inplace_scale_along_compressed_axis(x: Union[sp.csr_matrix, sp.csc_matrix], scale: np.ndarray) -> None:
+def inplace_scale_along_compressed_axis(A: Union[sp.csr_matrix, sp.csc_matrix], scale: np.ndarray) -> None:
     """
     Scale rows (columns) of a CSR (CSC) matrix by vector scale.
-    :param x: CSR (or CSC) matrix to be scaled
+    :param A: CSR (or CSC) matrix to be scaled
     :param scale: vector of scaling factors
     :return: None
     """
-    x.data *= np.repeat(scale, np.diff(x.indptr))
+    A.data *= np.repeat(scale, np.diff(A.indptr))
 
 
-def inplace_scale_along_uncompressed_axis(x: Union[sp.csr_matrix, sp.csc_matrix], scale: np.ndarray) -> None:
+def inplace_scale_along_uncompressed_axis(A: Union[sp.csr_matrix, sp.csc_matrix], scale: np.ndarray) -> None:
     """
     Scale rows (columns) of a CSC (CSR) matrix by vector scale.
-    :param x: CSR (or CSC) matrix to be scaled
+    :param A: CSR (or CSC) matrix to be scaled
     :param scale: vector of scaling factors
     :return: None
     """
-    x.data *= scale[x.indices]
+    A.data *= scale[A.indices]
